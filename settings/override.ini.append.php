ff902d953a669a4d6a9b2f8263ef7fef8f98e847
Brookins Consulting <info@brookinsconsulting.com>
2010-03-07 05:40:07 +0000

    Updated: Revised example override.ini settings for more accuracy.

--- settings/override.ini.append.php
+++ settings/override.ini.append.php
@@ -1,12 +1,12 @@
 <?php /* #?ini charset="utf-8"?
 
 # [email_notification_activation]
-# Source=node/view/email.tpl
+# Source=subscription_activation_email_notification.tpl
 # MatchFile=subscription_activation_email_notification.tpl
 # Subdir=templates
 
 # [email_notification_expiration]
-# Source=node/view/email.tpl
+# Source=subscription_expiration_email_notification.tpl
 # MatchFile=subscription_expiration_email_notification.tpl
 # Subdir=templates
 
